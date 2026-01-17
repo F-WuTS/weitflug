fe822752748333f619aacc8ca71dcd2ad35e463d
Alexander Lampalzer <sascha@localhost.systems>
2026-01-17 08:11:53 +0100

    Don't change attitude freq based on mode

--- src/main/fc/core.c
+++ src/main/fc/core.c
@@ -1000,14 +1000,14 @@ void processRxModes(timeUs_t currentTimeUs)
     }
 #endif
 
-    if (FLIGHT_MODE(ANGLE_MODE) || FLIGHT_MODE(HORIZON_MODE)) {
-        LED1_ON;
-        // increase frequency of attitude task to reduce drift when in angle or horizon mode
-        rescheduleTask(TASK_ATTITUDE, TASK_PERIOD_HZ(acc.sampleRateHz / (float)imuConfig()->imu_process_denom));
-    } else {
-        LED1_OFF;
-        rescheduleTask(TASK_ATTITUDE, TASK_PERIOD_HZ(100));
-    }
+    // if (FLIGHT_MODE(ANGLE_MODE) || FLIGHT_MODE(HORIZON_MODE)) {
+    //     LED1_ON;
+    //     // increase frequency of attitude task to reduce drift when in angle or horizon mode
+    //     rescheduleTask(TASK_ATTITUDE, TASK_PERIOD_HZ(acc.sampleRateHz / (float)imuConfig()->imu_process_denom));
+    // } else {
+    //     LED1_OFF;
+    //     rescheduleTask(TASK_ATTITUDE, TASK_PERIOD_HZ(100));
+    // }
 
     if (!IS_RC_MODE_ACTIVE(BOXPREARM) && ARMING_FLAG(WAS_ARMED_WITH_PREARM)) {
         DISABLE_ARMING_FLAG(WAS_ARMED_WITH_PREARM);
