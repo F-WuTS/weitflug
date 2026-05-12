3f92a4ec19011501335668b17dbb0d5e2c8049df
Jakob Buchsteiner <jakob.buchsteiner@gmail.com>
2026-05-12 18:42:44 +0200

    Set throttle control frequency to 50 Hz

--- src/main/flight/throttle_control.h
+++ src/main/flight/throttle_control.h
@@ -2,7 +2,7 @@
 
 #include "common/time.h"
 
-#define THROTTLE_CONTROL_TASK_RATE_HZ 100
+#define THROTTLE_CONTROL_TASK_RATE_HZ 50
 
 void throttleControlInit(void);
 void throttleControlUpdate(timeUs_t currentTimeUs);
